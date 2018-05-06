d46524355c3e8bd65a9706ecd1dbc917ba1abe4c
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-05-06 23:26:07 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,11 @@
 class Show < ActiveRecord::Base
 
-  def self.highest_rating 
+  def self.highest_rating
     maximum :rating
+  end
+
+  def self.most_popular_show
+    find_by(rating: highest_rating)
   end 
-  
+
 end
